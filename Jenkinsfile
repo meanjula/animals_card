9cc100350cdc1f548e3d930b63d7d4a1c72218f2
meanjula <anjula.ghale@gmail.com>
2022-03-09 13:01:58 +0200

    added jenkinsfile

---
+++ Jenkinsfile
@@ -0,0 +1,22 @@
+pipeline {
+    agent any 
+
+    stages {
+
+        stage ("Test") {
+            steps {
+                echo 'Echoing fake testing, nothing really happening...'
+            }
+        }
+                       {
+            steps {
+                echo 'Wanna-be building of the app'
+            }
+        }
+        stage ("Delpoy") {
+            steps {
+                echo 'Deploying in fantasy world'
+            }
+        }
+    }
+} 
--- src/animals/AnimalSingle.js
+++ src/animals/AnimalSingle.js
@@ -1,9 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useHistory } from "react-router-dom";
+import axios from "axios";
 
 function AnimalSingle(props) {
   let { animal } = useParams();
   let history = useHistory();
+  console.log(history);
+  // const [animalInfo, setAnimalInfo] = useState("");
+
+  const url = `/proxy/https://en.wikipedia.org/wiki/dog`;
+
+  const getAnimals = () => {
+    //promise.then(call back fun)
+    axios
+      .get(url)
+      .catch((error) => console.log(error))
+      .then((res) => {
+        const fetches = res;
+
+        console.log(fetches);
+      });
+  };
+
+  useEffect(() => {
+    getAnimals();
+    // eslint-disable-next-line
+  }, []);
   return (
     <div className="animal-single">
       <h3>This is page for {animal}</h3>
@@ -11,6 +33,7 @@ function AnimalSingle(props) {
         src={`https://source.unsplash.com/1600x900/?${animal}`}
         alt={animal}
       />
+      {/* <p>{animalInfo}</p> */}
       <button onClick={() => history.goBack()}>Back to animals</button>
     </div>
   );
